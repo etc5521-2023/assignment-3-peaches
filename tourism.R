4dea6091e907d880364aa0fde4eb222f4ca52cde
Arindam Baruah <abar0090@student.monash.edu>
2023-10-15 03:21:54 +1100

    Updated dataset as per new instructions

--- tourism.R
+++ tourism.R
@@ -10,31 +10,23 @@ library(lubridate)
 # Australia: Domestic Overnight Trips ('000) ----
 domestic_trips <- read_csv(
   "data/domestic_trips_2023-10-08.csv",
-  skip = 11,
-  col_names = c("Quarter", "Region", "Holiday", "Visiting", "Business", "Other"),
-  n_max = 6804
-)
+  skip = 9,
+  col_names = c("Quarter", "Region", "Holiday", "Visiting", "Business", "Other", "Total"),
+  n_max = 248056
+) %>% select(-X8)
 
 # fill NA in "Quarter" using the last obs
 fill_na <- domestic_trips %>%
   fill(Quarter, .direction = "down") %>%
   filter(Quarter != "Total")
 
-# separate State from "Region"
-state <- c(
-  "New South Wales", "Victoria", "Queensland", "South Australia",
-  "Western Australia", "Tasmania", "Northern Territory", "ACT"
-)
-state_na <- fill_na %>%
-  mutate(State = if_else(Region %in% state, Region, NA_character_)) %>%
-  fill(State, .direction = "up") %>%
-  filter(!(Region %in% state))
-
 # gather Stopover purpose of visit
-long_data <- state_na %>%
-  gather("Purpose", "Trips", Holiday:Other)
+long_data <- fill_na %>%
+  pivot_longer(cols=Holiday:Total,
+               names_to="Purpose",
+               values_to="Trips")
 
-# maniputate Quarter
+# manipulate Quarter
 qtr_data <- long_data %>%
   mutate(
     Quarter = paste(gsub(" quarter", "", Quarter), "01"),
@@ -43,6 +35,6 @@ qtr_data <- long_data %>%
 
 # convert to tsibble
 tourism <- qtr_data %>%
-  as_tsibble(key = c(Region, State, Purpose), index = Quarter)
+  as_tsibble(key = c(Region, Purpose), index = Quarter)
 usethis::use_data(tourism, overwrite = TRUE, compress = "xz")
 
